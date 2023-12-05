805b03157a25ca8be049c2b5fa474478f1b5e862
adityaraghuvanshi <68696650+adityaraghuvanshi@users.noreply.github.com>
2023-12-06 00:37:06 +0530

    initial changes

--- swaminarayansweets/index.js
+++ swaminarayansweets/index.js
@@ -1,56 +1,3 @@
-// const sliderContainer = document.querySelector(".slider-container");
-// const slideRight = document.querySelector(".right-slide");
-// const slideLeft = document.querySelector(".left-slide");
-// const slidesLength = slideRight.querySelectorAll("div").length;
-
-// let activeSlideIndex = 0;
-
-// let isMouseOverImages = false;
-
-// sliderContainer.addEventListener("mouseenter", () => {
-//     isMouseOverImages = true;
-// });
-
-// sliderContainer.addEventListener("mouseleave", () => {
-//     isMouseOverImages = false;
-// });
-
-// slideLeft.style.top = `-${(slidesLength - 1) * 100}vh`;
-
-// if (activeSlideIndex <= 3) {
-//     document.addEventListener("wheel", (event) => {
-//         if (event.deltaY > 0) {
-//             changeSlide("down");
-//         } else {
-//             changeSlide("up");
-//         }
-//     });
-// }
-
-// const changeSlide = (direction) => {
-//     if (isMouseOverImages) {
-//         const sliderHeight = sliderContainer.clientHeight;
-//         if (direction === "up") {
-//             activeSlideIndex++;
-//             if (activeSlideIndex > slidesLength - 1) {
-//                 activeSlideIndex = 0;
-//             }
-//         } else if (direction === "down") {
-//             activeSlideIndex--;
-//             if (activeSlideIndex < 0) {
-//                 activeSlideIndex = slidesLength - 1;
-//             }
-//         }
-
-//         slideRight.style.transform = `translateY(-${
-//             activeSlideIndex * sliderHeight
-//         }px)`;
-//         slideLeft.style.transform = `translateY(${
-//             activeSlideIndex * sliderHeight
-//         }px)`;
-//     }
-// };
-
 const cards = document.querySelectorAll(".card");
 
 function checkScroll() {
@@ -70,14 +17,14 @@ window.addEventListener("scroll", checkScroll);
 
 window.addEventListener("load", checkScroll);
 
-const navBar = document.querySelector(".navBar");
+const navbar = document.querySelector(".navbar");
 window.addEventListener("scroll", fixNav);
 
 function fixNav() {
-    if (window.scrollY > navBar.offsetHeight + 700) {
-        navBar.classList.add("active");
+    if (window.scrollY > navbar.offsetHeight + 700) {
+        navbar.classList.add("active");
     } else {
-        navBar.classList.remove("active");
+        navbar.classList.remove("active");
     }
 }
 
