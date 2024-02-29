78bdb9ca5200718975a516e1bdc68dcba961fba3
Aditya Raghuvanshi <raghuvanshiaditya2211@gmail.com>
2024-02-29 14:41:22 +0530

    change

--- pinterest-clone/app.js
+++ pinterest-clone/app.js
@@ -7,8 +7,10 @@ var expressSession = require("express-session");
 var indexRouter = require("./routes/index");
 var usersRouter = require("./routes/users");
 var passport = require("passport");
-var app = express();
 const flash = require("connect-flash");
+
+var app = express();
+
 // view engine setup
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
